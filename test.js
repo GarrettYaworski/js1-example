1f10ee2e5310854b0ef9d16e86f578f33c269e54
Garrett Yaworski <gy162134@gmail.com>
2023-01-14 14:43:55 -0600

    example completed

---
+++ test.js
@@ -0,0 +1,18 @@
+const myNum = 1;
+let myOtherNum = 5;
+
+let myBool = true;
+
+const myStr = 'My name is Garrett';
+
+const nullValue = null;
+
+let myUndefinedVar;
+
+myBool = true;
+
+if (myNum + myOtherNum > 5){
+  console.log('our number is big');
+} else {
+  console.log('we added small numbers');
+}
